33717aff8aae69a7ca6973365e1dac427f252d19
jojonoble1 <jojonoble1@gmail.com>
2024-02-01 14:21:12 -0700

    Update data.js
     - Updates services description

--- src/mock/data.js
+++ src/mock/data.js
@@ -18,7 +18,7 @@ export const aboutData = {
   paragraphOne:
     'I’m Josephine Noble, but you can call me Jojo. I’m a Salesforce Architect/Admin living in Missoula, Montana. I have a BA degree in Humanities with a focus on Film Studies from the University of Colorado -  Boulder. I currently work remotely for Slalom.',
   paragraphTwo:
-    'I am an experienced problem solver. At my current job you can find me leading new project initiatives and campaigns for different clients and industries, tracking down project requirements, and also providing both solutions and technical architectural support on a variety of apps.',
+    'I am an experienced problem solver. At my current job you can find me leading new project initiatives and campaigns for different clients and industries, tracking down project requirements, and also providing both solutions and technical architectural support on a variety of services.',
   paragraphThree:
     'In my free time, I am with my dogs Moose and Hank, enjoying the many outdoor activities the Big Sky country has to offer. My personal passions lie in all things exploration – whether it’s breaking ground on a newfound backpacking trip, floating the Missoula or North Fork Flathead River, or tinkering with computer programming.',
   resume: 'https://portfolio-file-bucket-2020.s3.amazonaws.com/Josephone+Noble+Resume.pdf',
